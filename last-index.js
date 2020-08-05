76eb05dad62f8e813ca37b846c5d3559ac7352f9
Paul Chen <paulchen647@gmail.com>
2020-08-05 23:28:56 +0000

    Added last-index.js for last index assignment

---
+++ last-index.js
@@ -0,0 +1,16 @@
+const lastIndexOf = function(array, num){
+  let last = -1;
+  for( let i = 0; i < array.length; i++){
+    if(array[i] === num){
+      last = i;
+    }
+  }
+  return last;
+}
+
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
